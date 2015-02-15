ed74bf6450ade37b2cb1c6f6a56c2e9a6e48238f
Caleb Martinez <cmart041@ucr.edu>
2015-02-14 22:07:43 -0800

    Finished script

--- Scripts/course_update/update_weekly_course.sh
+++ Scripts/course_update/update_weekly_course.sh
@@ -7,6 +7,14 @@ if [ -d  "$devcoursedir" ]
 
   echo "Connected to Garmin!"
 
+  # Wipe Garmin courses directory clean
+  rm -r $devcoursedir/*
+
+  # Copy updated weekly courses into device
+  cp $newcourse/* $devcoursedir
+
+  echo "Courses successfully updated."
+
 else
 
   echo "Unable to connect to Garmin device."
